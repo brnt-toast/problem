dceaa6c732988e867a8b77ece0078c1c2d794b7f
brnt-toast <yoyo.beerme@gmail.com>
2023-03-24 01:14:41 -0400

    beats: 98% speed, 27% memmemory

---
+++ 217-contains-dups.py
@@ -0,0 +1,6 @@
+"""
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+"""
+
+def solution(nums):
+    return len(nums) != len(set(nums))
